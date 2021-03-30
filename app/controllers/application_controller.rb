0fdaed7c32dc88fa1326aba2e53f4d300d890967
ka-jun <teraguchi51@gmail.com>
2021-03-31 06:44:21 +0900

    環境変数の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'teraguchi' && password == '33834'
+    end
+  end
 end
