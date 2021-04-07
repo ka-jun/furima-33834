58c11464573eaddc96e0592c78d5353b8ce45d41
ka-jun <teraguchi51@gmail.com>
2021-04-07 20:48:33 +0900

    user.rbにバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, unique: true, inclusion: {in: ['@']}
+  validates :encrypted_password, presence: true, {length : {minimum:6} }, format: {with: /\A[a-zA-Z0-9]+\z/, message: "半角英数字を使用してください"}
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true
 end
