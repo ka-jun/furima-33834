508ed8f1d04fe006fdc27f8d7e4796302ba7aaa7
ka-jun <teraguchi51@gmail.com>
2021-04-07 21:00:42 +0900

    バリデーションからemailとパスワード記述削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, unique: true, inclusion: {in: ['@']}
-  validates :encrypted_password, presence: true, {length : {minimum:6} }, format: {with: /\A[a-zA-Z0-9]+\z/, message: "半角英数字を使用してください"}
+  validates :encrypted_password, format: {with: /\A[a-zA-Z0-9]+\z/, message: "半角英数字を使用してください"}
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
