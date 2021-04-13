d2dc0e21847bfa049c56484d350479074b128579
ka-jun <teraguchi51@gmail.com>
2021-04-13 21:47:43 +0900

    ログイン機能記述漏れ入力

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private 
@@ -14,4 +13,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:birthday,:last_name,:first_name,:last_name_kana,:first_name_kana ])
   end
+
+  
 end
