3626e000ae9d390d6f029ff3f7d1cffb8b29634f
ka-jun <teraguchi51@gmail.com>
2021-04-14 21:53:15 +0900

    マイグレート修正

--- db/migrate/20210404215514_devise_create_users.rb
+++ db/migrate/20210404215514_devise_create_users.rb
@@ -4,16 +4,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      with_options null: false do
-      t.string :email,              unique: true
-      t.string :encrypted_password , default: ""
-      t.string :nickname           
-      t.date :birthday             
-      t.string :last_name          
-      t.string :first_name         
-      t.string :last_name_kana     
-      t.string :first_name_kana  
-      end  
+      t.string :email,              unique: true, null: false
+      t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,           null: false
+      t.date :birthday,             null: false
+      t.string :last_name,          null: false
+      t.string :first_name,         null: false
+      t.string :last_name_kana,     null: false
+      t.string :first_name_kana,    null: false
+
 
       ## Recoverable
       t.string   :reset_password_token
