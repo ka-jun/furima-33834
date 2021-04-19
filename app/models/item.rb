52cf243dabb47ba3df2d1c5750b15d30130cc683
ka-jun <teraguchi51@gmail.com>
2021-04-20 07:17:18 +0900

    エラーメッセージ表示、テストコード準備

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,7 @@ class Item < ApplicationRecord
     validates :shipping_charge_id
     validates :shipping_area_id
     validates :shipping_day_id
-    validates :price
+    validates :price, format: {with: /\A[0-9]+\z/, message: "Only numbers can be used." },
+    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "The number is invalid." }
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品登録/画像、商品名、商品の説明' do
+    context '画像、商品名、商品の説明の登録ができないとき' do
+      it '商品画像を1枚つけることが必須であること' do
+        @item.image = ''
+        @item.valid?
+        # expect(@item.errors.full_messages).to include(
