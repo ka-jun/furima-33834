d4d755965b3fbe9e7b987952939e9f2af6d5a529
ka-jun <77524367+ka-jun@users.noreply.github.com>
2021-04-26 21:21:20 +0900

    Revert "商品出品機能"

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-
   with_options presence: true do
     validates :nickname
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
     validates :birthday
 
     with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
-      validates :last_name
-      validates :first_name
+    validates :last_name
+    validates :first_name
     end
 
     with_options format: { with: /\A[ァ-ヶ]+\z/ } do
-      validates :last_name_kana
-      validates :first_name_kana
+    validates :last_name_kana
+    validates :first_name_kana
     end
   end
 end
