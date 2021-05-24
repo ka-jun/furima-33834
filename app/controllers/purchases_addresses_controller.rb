b25d2b5032603b3ecc7f4f786dd44869ae47306f
ka-jun <teraguchi51@gmail.com>
2021-05-24 21:48:53 +0900

    URL直接入力しても正しく誘導する実装

--- app/controllers/purchases_addresses_controller.rb
+++ app/controllers/purchases_addresses_controller.rb
@@ -1,9 +1,11 @@
 class PurchasesAddressesController < ApplicationController
   before_action :authenticate_user!, only: [:index, :new, :create]
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_item_index, only: [:index, :new, :create]
+
 
   def index
     @purchase_address = PurchaseAddress.new
-    @item = Item.find(params[:item_id])
   end
 
   def new
@@ -11,8 +13,6 @@ class PurchasesAddressesController < ApplicationController
   end
 
   def create
-    # binding.pry
-    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_address_params)
     if @purchase_address.valid?
       pay_item
@@ -31,15 +31,16 @@ class PurchasesAddressesController < ApplicationController
                   merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
-  # def address_params
-  #   params.require(:purchase_address).permit(:postal_code, :shipping_area_id, :city, 
-  #   :home_number, :building, :tel, :item_id).
-  #   merge(user_id: current_user.id, item_id: params[:item_id])
-  # end
-  # def purchase_params
-  #   params.require(:purchase_address).permit(:item_id, :teken).
-  #   merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token]))
-  # end
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_item_index
+    if @item.purchase.present?
+      # 商品情報に紐づいている購入履歴があったら
+      redirect_to root_path
+    end
+  end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   root to: "items#index"
   resources :items do
-    resources :purchases_addresses
+    resources :purchases_addresses, only: [:index, :new, :create]
   end
 end
